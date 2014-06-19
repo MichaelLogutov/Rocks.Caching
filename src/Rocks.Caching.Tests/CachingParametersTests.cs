882ff97060cc160039957827be2ae2967f5592d7
Michael Logutov <logutov@gmail.com>
2014-06-19 11:29:56 +0400

    Added Clone support for CachingParameters

---
+++ src/Rocks.Caching.Tests/CachingParametersTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+namespace Rocks.Caching.Tests
+{
+	[TestClass]
+	public class CachingParametersTests
+	{
+		[TestMethod]
+		public void Clone_ReturnsDeepClone ()
+		{
+			// arrange
+			var fixture = new Fixture ();
+
+			var source = fixture.Create<CachingParameters> ();
+
+
+			// act
+			var result = source.Clone ();
+
+
+			// assert
+			result.ShouldBeEquivalentTo (source);
+		}
+	}
+}
--- src/Rocks.Caching/CachingParameters.cs
+++ src/Rocks.Caching/CachingParameters.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 
 namespace Rocks.Caching
 {
 	/// <summary>
-	/// Specifying parameters for caching an object.
+	///     Specifying parameters for caching an object.
 	/// </summary>
-	public class CachingParameters
+	public sealed class CachingParameters
 	{
 		#region Constants
 
@@ -26,7 +28,7 @@ namespace Rocks.Caching
 		#region Construct
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="T:System.Object"/> class.
+		///     Initializes a new instance of the <see cref="T:System.Object" /> class.
 		/// </summary>
 		public CachingParameters (TimeSpan expiration, bool sliding = false, IEnumerable<string> dependencyKeys = null, CachePriority? priority = null)
 		{
@@ -36,54 +38,80 @@ namespace Rocks.Caching
 			this.priority = priority;
 		}
 
+
+		private CachingParameters ([NotNull] CachingParameters other)
+		{
+			if (other == null)
+				throw new ArgumentNullException ("other");
+
+			this.expiration = other.expiration;
+			this.sliding = other.sliding;
+
+			if (other.dependencyKeys != null)
+				this.dependencyKeys = other.dependencyKeys.ToList ();
+
+			this.priority = other.priority;
+		}
+
 		#endregion
 
 		#region Public properties
 
 		/// <summary>
-		/// (GET) Expiration time.
+		///     (GET) Expiration time.
 		/// </summary>
-		public TimeSpan Expiration
+		public TimeSpan Expiration { get { return this.expiration; } }
+
+
+		/// <summary>
+		///     (GET) True if current parameters represents sliding expiration.
+		/// </summary>
+		public bool Sliding { get { return this.sliding; } }
+
+		/// <summary>
+		///     (GET) Cached object priority.
+		/// </summary>
+		public CachePriority? Priority { get { return this.priority; } }
+
+
+		/// <summary>
+		///     (GET) List of dependency keys.
+		/// </summary>
+		public IEnumerable<string> DependencyKeys { get { return this.dependencyKeys; } }
+
+
+		/// <summary>
+		///     (GET) Returns true if no expiration set for current parameters.
+		/// </summary>
+		public bool NoCaching { get { return this.Expiration == TimeSpan.Zero; } }
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		///     Performs deep clone of the current object instance.
+		/// </summary>
+		public CachingParameters Clone ()
 		{
-			get { return this.expiration; }
+			return new CachingParameters (this);
 		}
 
 
 		/// <summary>
-		/// (GET) True if current parameters represents sliding expiration.
+		///     Returns a string that represents the current object.
 		/// </summary>
-		public bool Sliding
+		/// <returns>
+		///     A string that represents the current object.
+		/// </returns>
+		public override string ToString ()
 		{
-			get { return this.sliding; }
-		}
+			if (this.NoCaching)
+				return "no caching";
 
-		/// <summary>
-		/// (GET) Cached object priority.
-		/// </summary>
-		public CachePriority? Priority
-		{
-			get { return this.priority; }
-		}
+			var result = this.expiration + (this.sliding ? " sliding" : " absolute");
 
-
-		/// <summary>
-		/// (GET) List of dependency keys.
-		/// </summary>
-		public IEnumerable<string> DependencyKeys
-		{
-			get { return this.dependencyKeys; }
-		}
-
-
-		/// <summary>
-		/// (GET) Returns true if no expiration set for current parameters.
-		/// </summary>
-		public bool NoCaching
-		{
-			get
-			{
-				return this.Expiration == TimeSpan.Zero;
-			}
+			return result;
 		}
 
 		#endregion
